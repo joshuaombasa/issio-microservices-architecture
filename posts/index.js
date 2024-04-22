189dff7decd296fb27891e0f16b2b3fa2843da1f
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-22 20:32:40 +0300

    initial commit

---
+++ posts/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const axios = require('axios')
+const cors = require('cors')
+const { randomBytes } = require('crypto')
+const PORT = 4030
+
+const posts = {}
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/posts', (request, response) => {
+  response.json(posts)
+})
+
+app.post('/posts', async (request, response) => {
+  const { post } = request.body
+
+  const id = randomBytes(4).toString('hex')
+  posts[id] = { id, post }
+
+
+  try {
+    await axios.post('http://localhost:4035/events', {
+      type: 'postCreated',
+      data: { id, post }
+    })
+  } catch (error) {
+    console.error(error)
+  }
+
+  response.json(posts[id])
+})
+
+app.post('/events', (request, response) => {
+  const event = request.body
+  console.log(event)
+  response.json({})
+})
+
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`)
+})
