f990cfb822c2d697750b5e9e91eaab8036b1b96a
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-25 15:30:26 +0300

    working version

--- moderation/index.js
+++ moderation/index.js
@@ -10,23 +10,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/posts', (request, response) => {
-  response.json(posts)
-})
-
-app.post('/posts', async (request, response) => {
-  const { post } = request.body
-
-  const id = randomBytes(4).toString('hex')
-  posts[id] = { id, post }
-
-  await axios.post('http://localhost:4035/events', {
-    type: 'postCreated',
-    data: { id, post }
-  })
-
-  response.json(posts[id])
-})
 
 app.post('/events', async (request, response) => {
   const event = request.body
@@ -36,7 +19,7 @@ app.post('/events', async (request, response) => {
     const { postId, id, comment, status } = data
     const moderatedStatus = comment.includes('sex') ? 'rejected' : 'approved'
 
-    await axios.post('http://localhost:4035/events', {
+    await axios.post('http://event-bus-srv:4035/events', {
       type: 'commentModerated',
       data: { postId, id, comment, status: moderatedStatus }
     })
